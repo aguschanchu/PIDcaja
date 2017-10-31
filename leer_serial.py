454d1b8ba39a2b4b5357be7125dec6c3eb3f8317
Agustin Cianciulli <agusc@agusc.com.ar>
2017-10-31 11:44:00 -0300

    Typo

--- leer_serial.py
+++ leer_serial.py
@@ -13,7 +13,7 @@ import ast
 import redis
 import traceback
 arduino = serial.Serial("/dev/ttyACM0", 9600)
-outputdir = "/home/iteda/Dropbox/ITeDA/Sistema de control/"
+outputdir = "/home/iteda/Dropbox/ITeDA/Scripts/Lecroy/resultados/camara/"
 r = redis.StrictRedis(host='localhost', port=30000, db=0)
 
 ############ FIN DE CONFIG
@@ -22,60 +22,64 @@ if not r.exists('setpoint'):
 else:
     lastSetpoint = float(r.get('setpoint').decode('UTF-8'))
 
-if True:
-    while True:
-        #Guardamos los datos que entrega el arduino via serial
-        linea = arduino.readline().decode('utf-8')
-        print(linea)
-        with open(outputdir+"output.txt",'a') as file:
-            file.write(linea+"\n")
-        try:
-        	linea = ast.literal_eval(linea)
-        except:
-            linea = ""
-        if type(linea) == list:
-            #Veamos por casos, que tipo de informacion recibimos
-            if type(linea[0]) == int:
-    	        with open(outputdir+"sensor"+str(linea[0])+".txt",'a') as file:
-    	            file.write(str(linea[1])+','+str(linea[2])+"\n")
+while True:
+    #Guardamos los datos que entrega el arduino via serial
+    linea = arduino.readline().decode('utf-8')
+    print(linea)
+    with open(outputdir+"output.txt",'a') as file:
+        file.write(linea+"\n")
+    try:
+    	linea = ast.literal_eval(linea)
+    except:
+        linea = ""
+    if type(linea) == list:
+        #Veamos por casos, que tipo de informacion recibimos
+        if type(linea[0]) == int:
+	        with open(outputdir+"sensor"+str(linea[0])+".txt",'a') as file:
+	            file.write(str(linea[1])+','+str(linea[2])+"\n")
+	        if linea[0] == 7:
+	            r.set('control',linea[1])
+        elif linea[0] == "e":
+            with open(outputdir+"estado"+".txt",'a') as file:
+                lineaw = str(linea[1])
+                for i in range(2,7):
+                    lineaw=lineaw+','+str(linea[i])
+                file.write(lineaw+"\n")
+            #actualizamos sensor control
 
-            elif linea[0] == "e":
-                with open(outputdir+"estado"+".txt",'a') as file:
-                    lineaw = str(linea[1])
-                    for i in range(2,7):
-                        lineaw=lineaw+','+str(linea[i])
-                    file.write(lineaw+"\n")
-        #Leemos informacion de la DB, y enviamos, si es que hay cambios
-        if not r.exists('setpoint'):
-            print("No existe setpoint en DB, no vamos a cargarlo")
-        else:
-            newSetpoint=float(r.get('setpoint').decode('UTF-8'))
-            if newSetpoint != lastSetpoint:
-                mensaje='[setpoint,'+str(newSetpoint)+']'
-                print("Nuevo setpoint recibido")
-                newSetpoint=lastSetpoint
-                #Cambiamos correctamente el setpoint? Esperemos a que eso ocurra
-                cambiado = False
-                intentos = 31
-                while not cambiado:
-                    linea = arduino.readline().decode('utf-8')
-                    intentos = intentos + 1
-                    print(intentos)
-                    s="setpoint: "+str(newSetpoint)
-                    if 'setpoint' in linea:
-                        #es el lugar donde justo dice el setpoint
-                        recv = float(linea[10:14])
-                    else:
-                        recv = -10
-                    if recv == newSetpoint:
-                        cambiado = True
-                        print("OK")
-                    if intentos > 30:
-                        arduino.write(mensaje.encode())
-                        intentos=0
-                    print(linea)
-                    #Lo anterior no funciona, entonces, salvo que de problemas...
-                    cambiado=True
+    #Leemos informacion de la DB, y enviamos, si es que hay cambios
+    if not r.exists('setpoint'):
+        print("No existe setpoint en DB, no vamos a cargarlo")
+    else:
+        newSetpoint=float(r.get('setpoint').decode('UTF-8'))
+        if newSetpoint != lastSetpoint:
+            mensaje='[setpoint,'+str(newSetpoint)+']'
+            print("Nuevo setpoint recibido")
+            lastSetpoint=newSetpoint
+            #Cambiamos correctamente el setpoint? Esperemos a que eso ocurra
+            cambiado = False
+            intentos = 31
+            print(newSetpoint, lastSetpoint)
+            while not cambiado:
+                linea = arduino.readline().decode('utf-8')
+                intentos = intentos + 1
+                print(intentos)
+                s="setpoint: "+str(newSetpoint)
+                if 'setpoint' in linea:
+                    #es el lugar donde justo dice el setpoint
+                    recv = float(linea[10:14])
+                else:
+                    recv = -10
+                if recv == newSetpoint:
+                    cambiado = True
+                    print("OK")
+                if intentos > 30:
+                    arduino.write(mensaje.encode())
+                    intentos=0
+                print(linea)
+                #Lo anterior no funciona, entonces, salvo que de problemas...
+                cambiado=True
+
 
 '''
 except:
